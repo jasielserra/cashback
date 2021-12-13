7c1bb9a6b807025f3228f5498ef73f3f75be3e3e
jasiel Serra <jasiel_serra@hotmail.com>
2021-12-12 22:53:07 -0300

    Criando o modelo

--- cashback/sales/models.py
+++ cashback/sales/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class costumer(models.Model):
+    user = models.CharField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=("Costumer"))
+    name = models.CharField(verbose_name=("Name"), max_length=50)
+    document = models.CharField(verbose_name=("CPF"), max_length=11)
