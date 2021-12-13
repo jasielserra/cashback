5bbef40edae1410ebd84028bea6e1e75af7cd419
jasiel Serra <jasiel_serra@hotmail.com>
2021-12-13 08:16:36 -0300

    Implementando os modelos.

--- cashback/sales/models.py
+++ cashback/sales/models.py
@@ -4,7 +4,20 @@ from cashback import settings
 
 
 
-class costumer(models.Model):
-    user = models.CharField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=("Costumer"))
+class Costumer(models.Model):
+    user = models.CharField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(verbose_name=("Name"), max_length=50)
     document = models.CharField(verbose_name=("CPF"), max_length=11)
+
+
+class Product(models.Model):
+    type = models.PositiveSmallIntegerField(null=False)
+    value = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    qty = models.PositiveSmallIntegerField(null=False)
+
+
+class CashBack(models.Model):
+    costumer = models.ForeignKey(Costumer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product,)
+    sold_at = models.DateTimeField()
+    total = models.DecimalField(max_digits=10, decimal_places=2, null=False)
