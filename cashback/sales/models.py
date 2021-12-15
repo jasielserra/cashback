5cace9338aa382d28c924cf21c9110bbc1406ee5
jasiel Serra <jasiel_serra@hotmail.com>
2021-12-14 23:40:00 -0300

    Defininda a representação do CashBack do Costumer.

--- cashback/sales/models.py
+++ cashback/sales/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import MinLengthValidator
+from django.core.exceptions import ValidationError
 # Create your models here.
 
 class Costumer(models.Model):
@@ -25,3 +26,10 @@ class CashBack(models.Model):
     products = models.ManyToManyField(Product, verbose_name=_("Products"))
     sold_at = models.DateTimeField(default=timezone.now, verbose_name=_("Sale Date"))
     total = models.DecimalField(max_digits=10, decimal_places=2, null=False, verbose_name=_("Total"))
+
+    def __str__(self):
+        return self.costumer.name + " | " + str(self.total)
+
+    def sanitize(self):
+        if self.total < 0:
+            raise ValidationError("Cannot be negative")
--- cashback/urls.py
+++ cashback/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 
 from cashback.sales.views import CostumerViewSet
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('', CostumerViewSet),
 ]
